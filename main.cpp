f3438131dd8b69f2de5a0d1bfe3402970470fa58
Karamatdinov <cbcrb444555@gmail.com>
2025-10-06 20:53:22 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@
 int main(int argc, char** argv) {
 char* str = (char*)malloc(sizeof(char) * SIZE);
 printf("Введите своё имя:\n");
+<<<<<<< HEAD
 str = gets(str);
-printf("Привет, %s", str);
+puts("Привет");
+puts(str);
+=======
+scanf("%s", str);
+puts("Привет");
+puts(str);
+>>>>>>> c591d69 (Исправлено приветствие)
 return 0;
 }
